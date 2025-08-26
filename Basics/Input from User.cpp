02d06d62fb2f0edd4aa313f01ba7a129e756d62a
Peace <s.cihan.altinsoy@gmail.com>
2025-08-26 18:51:30 +0300

    Added three new programs

---
+++ Basics/Input from User.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i;
+    string fruit;
+
+    cout << "Enter a number: ";
+    cin >> i;
+    cout << "Enter your favorite fruit: ";
+    cin >> fruit;
+    cout << i << " " << fruit << " for you then!";
+
+
+}
